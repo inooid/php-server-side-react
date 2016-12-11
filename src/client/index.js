6835453f85a21f86a3acc67c44fbea800066367f
Boyd Dames <JustDrawCats@gmail.com>
2016-12-11 22:23:03 +0100

    Add client app

---
+++ src/client/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from 'containers/app';
+
+// Fixes nested objects being passed down to props
+function parseInitialProps(props) {
+  const { pages } = props;
+
+  return Object.assign({}, props, { pages: JSON.parse(pages) });
+}
+
+const appNode = document.getElementById('app');
+
+ReactDOM.render(
+  React.createElement(App, parseInitialProps(window.__REACT_INITIAL_PROPS)),
+  appNode
+);
