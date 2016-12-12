dd4595b7430f69e1d8d9686faa6513782f53959c
Boyd Dames <JustDrawCats@gmail.com>
2016-12-12 10:07:36 +0100

    Remove hotfix because bad json_encoding

--- src/client/index.js
+++ src/client/index.js
@@ -3,16 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from 'containers/app';
 
-// Fixes nested objects being passed down to props as objects, instead of strings
-function parseInitialProps(props) {
-  const { pages } = props;
-
-  return Object.assign({}, props, { pages: JSON.parse(pages) });
-}
-
 const appNode = document.getElementById('app');
 
 ReactDOM.render(
-  React.createElement(App, parseInitialProps(window.__REACT_INITIAL_PROPS)),
+  React.createElement(App, window.__REACT_INITIAL_PROPS),
   appNode
 );
